e0049b15aced8161e8d866afff2f5fc10da58297
annibuliful <lagmanmc@gmail.com>
2023-03-25 02:51:48 +0700

    feat: canvas element

---
+++ src/components/Canvas.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef } from 'react';
+import { fabric } from 'fabric';
+import { useCardCanvas } from '@/hooks/useCardCanvas';
+const CardCanvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const cardCanvasRef = useCardCanvas();
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const width = canvasRef.current?.clientWidth;
+    const height = canvasRef.current?.clientHeight;
+
+    const canvas = new fabric.Canvas(canvasRef.current, {
+      height,
+      width,
+      enableRetinaScaling: false,
+      selection: false,
+      skipOffscreen: true,
+      renderOnAddRemove: false,
+    });
+    // @ts-ignore
+    cardCanvasRef.current = canvas;
+
+    // @ts-ignore
+    fabric.perfLimitSizeTotal = 2097152 * 4;
+    fabric.textureSize = 8129;
+
+    const resize = () => {
+      canvas.setDimensions({
+        width,
+        height,
+      });
+
+      canvas.requestRenderAll();
+    };
+    resize();
+
+    window.addEventListener('resize', resize);
+    return () => {
+      canvas.dispose();
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
+
+  return (
+    <canvas
+      touch-action="none"
+      ref={canvasRef}
+      id="fabricCanvas"
+      style={{
+        height: '100%',
+        width: '100%',
+      }}
+    />
+  );
+};
+
+export default CardCanvas;
