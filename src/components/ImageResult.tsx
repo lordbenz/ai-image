ee1823505b8beaa8826ad80cf8f4fc4cca5490e8
annibuliful <lagmanmc@gmail.com>
2023-03-25 02:50:40 +0700

    feat: image preview

--- src/components/ImageResult.tsx
+++ src/components/ImageResult.tsx
@@ -1,34 +1,14 @@
-import React from "react";
-import Image from "next/image";
-import StaffTemplate from "../assets/staff.png";
-import AttendeeTemplate from "../assets/attendee.png";
+import React from 'react';
 type ImageResultProps = {
   imagePreview: string | null | undefined;
 };
 
 const ImageResult = ({ imagePreview }: ImageResultProps) => {
-  return (
-    <div className="mt-4">
-      {imagePreview ? (
-        <img src={imagePreview} alt="Image Preview" />
-      ) : (
-        <div className="flex space-x-10">
-          <Image
-            src={StaffTemplate}
-            alt="Staff Template"
-            width={250}
-            height={350}
-          />
-          <Image
-            src={AttendeeTemplate}
-            alt="Staff Template"
-            width={250}
-            height={350}
-          />
-        </div>
-      )}
-    </div>
-  );
+  if (imagePreview) {
+    return <img src={imagePreview} alt="Image Preview" />;
+  }
+
+  return null;
 };
 
 export default ImageResult;
