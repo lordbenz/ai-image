c0532ac9bfb028061b69e3cbdeceb1d66f2d93ed
annibuliful <lagmanmc@gmail.com>
2023-03-25 01:40:40 +0700

    feat: check task status

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,26 +4,41 @@ import { AIForm } from '@/components/AIForm';
 import ImageResult from '@/components/ImageResult';
 import { useState } from 'react';
 import { FormData } from '../types/types';
+import { createAiTask, delay, getAiTaskResult } from '@/services/ai';
 
 export default function Home() {
   const [imagePreview, setImagePreview] = useState<string | null>(
     null
   );
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = (data: FormData) => {
+  const handleSubmit = async (data: FormData) => {
     if (!data) return;
 
-    console.log({ submitData: data });
-    // TODO: Set imagePreview to result
+    const createdTaskResponse = await createAiTask(data.imageUrl);
+    setIsLoading(true);
+    let isFinished = false;
 
-    const result = generatedAIImageURL(data.image as File);
-    setImagePreview(result);
-  };
-
-  const generatedAIImageURL = (file: File) => {
-    // TODO: Generate AI Image
-
-    return 'https://via.placeholder.com/250x350';
+    while (!isFinished) {
+      try {
+        await delay(2000);
+        const taskResultResponse = await getAiTaskResult(
+          createdTaskResponse.batchTaskId
+        );
+        if (
+          (!taskResultResponse.query &&
+            taskResultResponse.taskStatus === 22) ||
+          taskResultResponse.images[0].taskStatus === 22
+        ) {
+          isFinished = true;
+          setImagePreview(taskResultResponse.images[0].original);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        isFinished = true;
+        setIsLoading(false);
+      }
+    }
   };
 
   return (
@@ -32,7 +47,11 @@ export default function Home() {
         <HeadSection />
         <div className="flex flex-col md:flex-row w-full m-10">
           <div className="flex w-full md:w-1/2 m-10">
-            <AIForm onSubmit={handleSubmit} />
+            <AIForm
+              onSubmit={handleSubmit}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+            />
           </div>
           <div className="flex w-full md:w-1/2 m-10">
             <ImageResult imagePreview={imagePreview} />
