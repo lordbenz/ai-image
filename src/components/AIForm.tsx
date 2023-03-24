07858a99392366c6a68c01a3ae8d6b405cd2929c
annibuliful <lagmanmc@gmail.com>
2023-03-25 01:41:19 +0700

    feat: loading state

--- src/components/AIForm.tsx
+++ src/components/AIForm.tsx
@@ -5,9 +5,15 @@ import { FormData } from '../types/types';
 
 type AIFormProps = {
   onSubmit: (data: FormData) => void;
+  isLoading: boolean;
+  setIsLoading: (status: boolean) => void;
 };
 
-export const AIForm = ({ onSubmit }: AIFormProps) => {
+export const AIForm = ({
+  onSubmit,
+  isLoading,
+  setIsLoading,
+}: AIFormProps) => {
   const [name, setName] = useState('');
   const [role, setRole] = useState('');
   const [imageFile, setImageFile] = useState<File>();
@@ -17,6 +23,7 @@ export const AIForm = ({ onSubmit }: AIFormProps) => {
   ) => {
     event.preventDefault();
     if (!imageFile) return;
+    setIsLoading(true);
     const uploadImageResponse = await uploadImage(imageFile);
     const url = new URL(uploadImageResponse.data.data.url);
 
@@ -96,7 +103,35 @@ export const AIForm = ({ onSubmit }: AIFormProps) => {
         }
         disabled={disabled}
       >
-        Submit
+        <div className="flex">
+          {isLoading ? (
+            <>
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Processing...
+            </>
+          ) : (
+            'Generate'
+          )}
+        </div>
       </button>
     </form>
   );
