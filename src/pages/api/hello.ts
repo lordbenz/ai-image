8f217091cd308968e8b9c7d3a9979ec19c63ca21
annibuliful <lagmanmc@gmail.com>
2023-03-25 04:17:42 +0700

    feat: write file

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { nanoid } from 'nanoid';
+import fs from 'fs/promises';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  await fs.writeFile(
+    `info/${nanoid()}.json`,
+    JSON.stringify(req.body)
+  );
+  res.status(200).json({ name: 'John Doe' });
 }
